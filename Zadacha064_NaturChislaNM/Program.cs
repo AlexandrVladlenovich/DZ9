982dd0adff769210e2fce1979e736a1f3a5e6941
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-09-04 00:37:17 +0500

    add Zadacha064_NaturChislaNM

---
+++ Zadacha064_NaturChislaNM/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите (от 0 до 9) число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите (от 0 до 9) число n (n должно быть больше m):");
+int n = int.Parse(Console.ReadLine());
+void PrintNumber(int m, int n)
+{
+    if (n < m) return;
+    PrintNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+Console.WriteLine("Результат: ");
+PrintNumber(m, n);
