4f815d32c111edd1985100b7c6b8a79e041fb8d2
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-09-04 00:55:28 +0500

    add Zadacha064_NaturChislaNM

---
+++ Zadacha068_SumNaturChiselMN/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число m (должно быть > 0): ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n (n должно быть больше m):");
+int n = int.Parse(Console.ReadLine());
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine("Результат:");
+Console.WriteLine(Sum(m, n));
